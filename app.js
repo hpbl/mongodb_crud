086118ea108f8e437f3bc3e54f2d264d08aa6e8e
Hilton Pintor <hiltonpintor@gmail.com>
2019-01-28 13:20:30 -0300

    User Input Validation

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.use(bodyParser.json());
 
 const path = require('path');
 
+const Joi = require('joi');
+const schema = Joi.object().keys({
+  todo: Joi.string().required(),
+});
+
 const db = require('./db');
 const collection = 'todo';
 
@@ -42,15 +47,26 @@ app.put('/:id', (req, res) => {
   })
 });
 
-app.post('/', (req, res) => {
+app.post('/', (req, res, next) => {
   const userInput = req.body;
-  db.getDB().collection(collection).insertOne(userInput, (err, result) => {
+
+  Joi.validate(userInput, schema, (err, result) => {
     if (err) {
-      console.log(err);
+      const error = new Error("Invalid Input");
+      error.status = 400;
+      next(error);
     } else {
-      res.json({result: result, document: result.ops[0]});
+      db.getDB().collection(collection).insertOne(userInput, (err, result) => {
+        if (err) {
+          const error = new Error("Failed to insert Todo Document");
+          error.status = 400;
+          next(error);
+        } else {
+          res.json({result: result, document: result.ops[0], msg: "Successfully inserted Todo!", error: null});
+        }
+      });
     }
-  });
+  })
 });
 
 app.delete('/:id', (req, res) => {
@@ -64,6 +80,14 @@ app.delete('/:id', (req, res) => {
       res.json(result);
     }
   })
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status).json({
+    error: {
+      message: err.message
+    }
+  });
 });
 
 db.connect((err) => {
